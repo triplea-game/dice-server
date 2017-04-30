7c6bd5c9bdc20b6b9191e07331cd891b9284b91a
Steven Soloff <steven.soloff@gmail.com>
2017-04-29 21:19:37 -0400

    Initial import of MARTI dice server

---
+++ tripleawarclub.org/public_html/dice/MARTI_verify.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MARTI Verifyer</title>
+	</head>
+	<body>
+		<?php
+		include_once('dice.class.php');
+
+		if(!isset($_GET["iv"]) || !isset($_GET["enc"])){
+			exit("error: You used an invalid link!");
+		}
+
+		$iv = $_GET["iv"];
+		$enc = $_GET["enc"];
+
+		$dice = new dice();
+
+		$outputArray = $dice->decrypt_data(null, $iv, $enc);
+
+		echo "Dice were authentic: " . $outputArray['dice'];
+		?>
+	</body>
+</html>
