a424fcefc5c65697653db662960f538dbfc3d2ea
Steven Soloff <steven.soloff@gmail.com>
2017-05-01 13:58:17 -0400

    Add script to initialize MARTI database schema

---
+++ build/mysql/marti-init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `dice_emails` (
+  `registered_email` varchar(255) NOT NULL,
+  UNIQUE KEY `registered_email` (`registered_email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `dice_table` (
+  `request_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UUID` varchar(36) NOT NULL,
+  `dice` varchar(256) NOT NULL,
+  `subject` varchar(70) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`request_ID`),
+  KEY `UUID` (`UUID`)
+) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `pending_validations` (
+  `email` varchar(255) NOT NULL,
+  `validation_key` char(32) NOT NULL,
+  `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `IP` int(10) unsigned NOT NULL,
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
