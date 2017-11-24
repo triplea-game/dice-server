46913c76f92e872425142f00e56e9360fac21573
Steven Soloff <steven.soloff@gmail.com>
2017-11-23 23:33:27 -0500

    Resync with latest production code
    Somehow the production code got out of sync with the version used to
    initialize this repository.  This commit ensures the repo code is in
    sync with the latest production code while maintaining the changes
    previously applied to remove hard-coded sensitive information from the
    source code, as well as to allow MARTI to run from a Docker container.

--- dice/marti_mod.php
+++ dice/marti_mod.php
@@ -3,12 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-	<head>
-		<title>	M.A.R.T.I. Server --> "more accurate rolls than irony"- server</title>
-	</head>
-	<body>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>	M.A.R.T.I. Server --> "more accurate rolls than irony"- server</title>
+</head>
+<body>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -58,6 +59,8 @@ catch(exception $e) {
 	exit($e->getMessage());
 }
 
+
+
 //create dice
 $output['dice'] = $dice->createdice($numdice, $numsides);
 
@@ -74,5 +77,6 @@ $dice->sendEmail($output['emails'], $output['subject'], $output['dice'], $enc_ar
 echo "your dice are: " . $output['dice'] . "<p><p>";
 
 ?>
-	</body>
+
+</body>
 </html>
--- dice/unsubscribe.php
+++ dice/unsubscribe.php
@@ -1,45 +1,46 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		<title>Unsubscribe from MARTI dice services</title>
-	</head>
-	<body>
-		<?php
-		$email = filter_input( INPUT_POST, "email", FILTER_SANITIZE_EMAIL );
-		if( is_null( $email ) || $email===false ){ $email = filter_input( INPUT_GET, "email", FILTER_SANITIZE_EMAIL ); }
-		if( is_null( $email ) || $email===false ) {
-		// template for first load
-		?>
-			<form method="POST">
-				Enter your email here to unsubscribe your email from MARTI dice services:
-				<br>
-				<input type="text" name="email" width="40">
-				<br>
-				<input type="submit" value="unsubscribe">
-			</form>
-		<?php
-		}
-		else {
-		// template for handling postbacks
-			require_once("dice.class.php");
-			$dice = new dice;
+<head>
+	<title>Unsubscribe from MARTI dice services</title>
+</head>
+<body>
+<?php
+$email = filter_input( INPUT_POST, "email", FILTER_SANITIZE_EMAIL );
+if( is_null( $email ) || $email===false ){ $email = filter_input( INPUT_GET, "email", FILTER_SANITIZE_EMAIL ); }
+if( is_null( $email ) || $email===false ) {
+// template for first load
+?>
+	<form method="post" action="unsubscribe.php">
+		Enter your email here to unsubscribe your email from MARTI dice services:
+		<br/>
+		<input type="text" name="email" width="40" />
+		<br/>
+		<input type="submit" value="unsubscribe">
+	</form>
+<?php
+}
+else {
+// template for handling postbacks
+	require_once("dice.class.php");
+	$dice = new dice;
 
-			if(! $dice->checkIfMailIsRegistered($email)){
-				exit("This email is not registered, or was never validated.");
-			}
+	if( ! dice::checkEmail($email) )
+		exit("Invalid email format! Please enter a valid email address");
 
-			$sql = "DELETE FROM dice_emails WHERE registered_email=?";
+	if(! $dice->checkIfMailIsRegistered($email))
+		exit("This email is not registered, or was never validated.");
 
-			if( $sth = $dice->dbconn->prepare( $sql ) ){
-			  $sth->bind_param('s',$email);
-			  $sth->execute() or trigger_error($mysqli->error);
-			} else {
-			  echo "A DB error has occured, please contact an admin. (2-"; var_dump( $dice->dbconn->errno );
-			  echo ")";
-			  exit;
-			}
-			echo "Your email was successfully removed. You will no longer receive dice emails.";
-		}
-		?>
-	</body>
+	$sql = "DELETE FROM dice_emails WHERE registered_email=?";
+
+if( $sth = $dice->dbconn->prepare( $sql ) ){
+  $sth->bind_param('s',$email);
+  $sth->execute() or trigger_error($mysqli->error);
+} else {
+  echo "A DB error has occured, please contact an admin. (2-"; var_dump( $dice->dbconn->errno ); echo ")";exit;
+}
+
+	echo "Your email was successfully removed. You will no longer receive dice emails.";
+}
+?>
+
+</body>
 </html>
